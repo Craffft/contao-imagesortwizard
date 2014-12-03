d513a120cd758ea8f49d68fb4f4794d46b1d335e
iCodr8 <icodr8@gmail.com>
2014-12-03 21:10:03 +0100

    refactored to psr-2

--- system/modules/imagesortwizard/widgets/ImageSortWizard.php
+++ system/modules/imagesortwizard/widgets/ImageSortWizard.php
@@ -11,12 +11,10 @@
  * @copyright  Daniel Kiesel 2012-2014
  */
 
-
 /**
  * Namespace
  */
 namespace ImageSortWizard;
-
 
 /**
  * Class ImageSortWizard
@@ -27,57 +25,52 @@ namespace ImageSortWizard;
  */
 class ImageSortWizard extends \Widget
 {
+    /**
+     * Submit user input
+     * @var boolean
+     */
+    protected $blnSubmitInput = true;
 
-	/**
-	 * Submit user input
-	 * @var boolean
-	 */
-	protected $blnSubmitInput = true;
+    /**
+     * Template
+     * @var string
+     */
+    protected $strTemplate = 'be_widget';
 
-	/**
-	 * Template
-	 * @var string
-	 */
-	protected $strTemplate = 'be_widget';
+    /**
+     * Add specific attributes
+     * @param string
+     * @param mixed
+     */
+    public function __set($strKey, $varValue)
+    {
+        switch ($strKey) {
+            case 'mandatory':
+                $this->arrConfiguration['mandatory'] = $varValue ? true : false;
+                break;
 
+            default:
+                parent::__set($strKey, $varValue);
+                break;
+        }
+    }
 
-	/**
-	 * Add specific attributes
-	 * @param string
-	 * @param mixed
-	 */
-	public function __set($strKey, $varValue)
-	{
-		switch ($strKey)
-		{
-			case 'mandatory':
-				$this->arrConfiguration['mandatory'] = $varValue ? true : false;
-				break;
-
-			default:
-				parent::__set($strKey, $varValue);
-				break;
-		}
-	}
-
-
-	/**
-	 * Trim values
-	 * @param mixed
-	 * @return mixed
-	 */
-	public function validator($varInput)
-	{
-		if (is_array($varInput))
-		{
-			return parent::validator($varInput);
-		}
+    /**
+     * Trim values
+     * @param mixed
+     * @return mixed
+     */
+    public function validator($varInput)
+    {
+        if (is_array($varInput)) {
+            return parent::validator($varInput);
+        }
 
         $varInput = $this->fixUuidToBinary($varInput);
         $varInput = trim($varInput);
 
-		return parent::validator($varInput);
-	}
+        return parent::validator($varInput);
+    }
 
     protected function fixUuidToBinary($strUuid)
     {
@@ -88,171 +81,152 @@ class ImageSortWizard extends \Widget
         return $strUuid;
     }
 
-	/**
-	 * Generate the widget and return it as string
-	 * @return string
-	 */
-	public function generate()
-	{
+    /**
+     * Generate the widget and return it as string
+     * @return string
+     */
+    public function generate()
+    {
         $return = '';
-		$arrButtons = array('up', 'down');
-		$strCommand = 'cmd_' . $this->strField;
+        $arrButtons = array('up', 'down');
+        $strCommand = 'cmd_'.$this->strField;
 
-		// Add JavaScript and css
-		if (TL_MODE == 'BE')
-		{
-			$GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/imagesortwizard/assets/js/imagesortwizard.min.js';
-		    $GLOBALS['TL_CSS'][] = 'system/modules/imagesortwizard/assets/css/imagesortwizard.min.css|screen';
-		}
+        // Add JavaScript and css
+        if (TL_MODE == 'BE') {
+            $GLOBALS['TL_JAVASCRIPT'][] = 'system/modules/imagesortwizard/assets/js/imagesortwizard.min.js';
+            $GLOBALS['TL_CSS'][] = 'system/modules/imagesortwizard/assets/css/imagesortwizard.min.css|screen';
+        }
 
-		// Change the order
-		if ($this->Input->get($strCommand) && is_numeric($this->Input->get('cid')) && $this->Input->get('id') == $this->currentRecord)
-		{
-			$this->import('Database');
+        // Change the order
+        if ($this->Input->get($strCommand) && is_numeric($this->Input->get('cid')) && $this->Input->get('id') == $this->currentRecord) {
+            $this->import('Database');
 
-			switch ($this->Input->get($strCommand))
-			{
-				case 'up':
-					$this->varValue = array_move_up($this->varValue, $this->Input->get('cid'));
-					break;
+            switch ($this->Input->get($strCommand)) {
+                case 'up':
+                    $this->varValue = array_move_up($this->varValue, $this->Input->get('cid'));
+                    break;
 
-				case 'down':
-					$this->varValue = array_move_down($this->varValue, $this->Input->get('cid'));
-					break;
-			}
+                case 'down':
+                    $this->varValue = array_move_down($this->varValue, $this->Input->get('cid'));
+                    break;
+            }
 
-			$this->Database->prepare("UPDATE " . $this->strTable . " SET " . $this->strField . "=? WHERE id=?")
-						   ->execute(serialize($this->varValue), $this->currentRecord);
+            $this->Database->prepare("UPDATE ".$this->strTable." SET ".$this->strField."=? WHERE id=?")
+                           ->execute(serialize($this->varValue), $this->currentRecord);
 
-			$this->redirect(preg_replace('/&(amp;)?cid=[^&]*/i', '', preg_replace('/&(amp;)?' . preg_quote($strCommand, '/') . '=[^&]*/i', '', $this->Environment->request)));
-		}
+            $this->redirect(preg_replace('/&(amp;)?cid=[^&]*/i', '', preg_replace('/&(amp;)?'.preg_quote($strCommand, '/').'=[^&]*/i', '', $this->Environment->request)));
+        }
 
-		$tabindex = 0;
-		$return .= '<div id="ctrl_'.$this->strId.'" class="tl_imagesortwizard">';
-			$return .= '<ul class="sortable">';
+        $tabindex = 0;
+        $return .= '<div id="ctrl_'.$this->strId.'" class="tl_imagesortwizard">';
+        $return .= '<ul class="sortable">';
 
-			// Get sort Images
-			$this->sortImages = $this->getSortedImages();
+            // Get sort Images
+            $this->sortImages = $this->getSortedImages();
 
-			// Make sure there is at least an empty array
-			if (!is_array($this->varValue) || count($this->varValue) < 1)
-			{
-				$this->varValue = array();
-			}
+            // Make sure there is at least an empty array
+            if (!is_array($this->varValue) || count($this->varValue) < 1) {
+                $this->varValue = array();
+            }
 
-			// Set var sortImages as array if there is none
-			if (!is_array($this->sortImages) || count($this->sortImages) < 1)
-			{
-				$this->sortImages = array();
-			}
+            // Set var sortImages as array if there is none
+            if (!is_array($this->sortImages) || count($this->sortImages) < 1) {
+                $this->sortImages = array();
+            }
 
-			// Set var value
-			$newVarValue = array();
+            // Set var value
+            $newVarValue = array();
 
-			// Remove old Images
-			if(count($this->varValue) > 0)
-			{
-				$objFiles = (\FilesModel::findMultipleByUuids($this->varValue));
+            // Remove old Images
+            if (count($this->varValue) > 0) {
+                $objFiles = (\FilesModel::findMultipleByUuids($this->varValue));
 
-				if($objFiles !== null)
-				{
-					while($objFiles->next())
-					{
-						if (in_array($objFiles->uuid, $this->sortImages) || in_array($objFiles->id, $this->sortImages)) // Backwards compatibility (id)
-						{
-							$newVarValue[] = $objFiles->uuid;
-						}
-					}
-				}
-			}
+                if ($objFiles !== null) {
+                    while ($objFiles->next()) {
+                        if (in_array($objFiles->uuid, $this->sortImages) || in_array($objFiles->id, $this->sortImages)) {
+                            // Backwards compatibility (id)
 
-			// Set newVarValue in varValue
-			$this->varValue = $newVarValue;
+                            $newVarValue[] = $objFiles->uuid;
+                        }
+                    }
+                }
+            }
 
-			// Add new Images
-			if(count($this->sortImages) > 0)
-			{
-				$objFiles = (\FilesModel::findMultipleByUuids($this->sortImages));
+            // Set newVarValue in varValue
+            $this->varValue = $newVarValue;
 
-				if($objFiles !== null)
-				{
-					while($objFiles->next())
-					{
-						if (!in_array($objFiles->uuid, $this->varValue))
-						{
-							$this->varValue[] = $objFiles->uuid;
-						}
-					}
-				}
-			}
+            // Add new Images
+            if (count($this->sortImages) > 0) {
+                $objFiles = (\FilesModel::findMultipleByUuids($this->sortImages));
 
-			$objFiles = (\FilesModel::findMultipleByUuids($this->varValue));
+                if ($objFiles !== null) {
+                    while ($objFiles->next()) {
+                        if (!in_array($objFiles->uuid, $this->varValue)) {
+                            $this->varValue[] = $objFiles->uuid;
+                        }
+                    }
+                }
+            }
 
-			if($objFiles !== null)
-			{
-				$i = 0;
-				$rows = ($objFiles->count()-1);
+        $objFiles = (\FilesModel::findMultipleByUuids($this->varValue));
 
-				while($objFiles->next())
-				{
-					$objFile = new \File($objFiles->path);
+        if ($objFiles !== null) {
+            $i = 0;
+            $rows = ($objFiles->count()-1);
 
-					// Generate thumbnail
-					if ($objFile->isGdImage && $objFile->height > 0)
-					{
-						if ($GLOBALS['TL_CONFIG']['thumbnails'] && $objFile->height <= $GLOBALS['TL_CONFIG']['gdMaxImgHeight'] && $objFile->width <= $GLOBALS['TL_CONFIG']['gdMaxImgWidth'])
-					    {
-					    	$_width = ($objFile->width < 80) ? $objFile->width : 80;
-						    $_height = ($objFile->height < 60) ? $objFile->height : 60;
+            while ($objFiles->next()) {
+                $objFile = new \File($objFiles->path);
 
-					    	$thumbnail = '<img src="' . TL_FILES_URL . $this->getImage($objFiles->path, $_width, $_height, 'center_center') . '" alt="thumbnail">';
-					    }
-					}
+                    // Generate thumbnail
+                    if ($objFile->isGdImage && $objFile->height > 0) {
+                        if ($GLOBALS['TL_CONFIG']['thumbnails'] && $objFile->height <= $GLOBALS['TL_CONFIG']['gdMaxImgHeight'] && $objFile->width <= $GLOBALS['TL_CONFIG']['gdMaxImgWidth']) {
+                            $_width = ($objFile->width < 80) ? $objFile->width : 80;
+                            $_height = ($objFile->height < 60) ? $objFile->height : 60;
 
-					$return .= '<li>';
-						$return .= $thumbnail;
-						$return .= '<input type="hidden" name="'.$this->strId.'[]" class="tl_text" tabindex="'.++$tabindex.'" value="'.specialchars(\String::binToUuid($objFiles->uuid)).'"' . $this->getAttributes() . '>';
-					$return .= '</li>';
+                            $thumbnail = '<img src="'.TL_FILES_URL.$this->getImage($objFiles->path, $_width, $_height, 'center_center').'" alt="thumbnail">';
+                        }
+                    }
 
-					$i++;
-				}
-			}
+                $return .= '<li>';
+                $return .= $thumbnail;
+                $return .= '<input type="hidden" name="'.$this->strId.'[]" class="tl_text" tabindex="'.++$tabindex.'" value="'.specialchars(\String::binToUuid($objFiles->uuid)).'"'.$this->getAttributes().'>';
+                $return .= '</li>';
 
-			$return .= '</ul>';
-		$return .= '</div>';
+                $i++;
+            }
+        }
 
-		return $return;
-	}
+        $return .= '</ul>';
+        $return .= '</div>';
 
+        return $return;
+    }
 
-	public function getSortedImages()
-	{
-		if (!$this->sortfiles)
-		{
-			return false;
-		}
+    public function getSortedImages()
+    {
+        if (!$this->sortfiles) {
+            return false;
+        }
 
-		// Set arrays
-		$arrSortfiles = array();
+        // Set arrays
+        $arrSortfiles = array();
 
-		// Import
-		$this->import('Database');
-		$this->import('Files');
+        // Import
+        $this->import('Database');
+        $this->import('Files');
 
-		// Get Sortfiles
-		$objSortfiles = $this->Database->prepare("SELECT " . $this->sortfiles . " FROM " . $this->strTable . " WHERE id=?")
-										->execute($this->currentRecord);
+        // Get Sortfiles
+        $objSortfiles = $this->Database->prepare("SELECT ".$this->sortfiles." FROM ".$this->strTable." WHERE id=?")
+                                        ->execute($this->currentRecord);
 
-		// Fetch
-		$arrSortfiles = $objSortfiles->fetchAssoc();
-		$arrUuids = deserialize($arrSortfiles[$this->sortfiles]);
+        // Fetch
+        $arrSortfiles = $objSortfiles->fetchAssoc();
+        $arrUuids = deserialize($arrSortfiles[$this->sortfiles]);
 
-		// Create new object from ImageSorter and get unsorted files
-		$objImageSorter = new ImageSorter($arrUuids, $this->extensions);
-		$objImageSorter->sortImagesBy('custom', 'ASC');
+        // Create new object from ImageSorter and get unsorted files
+        $objImageSorter = new ImageSorter($arrUuids, $this->extensions);
+        $objImageSorter->sortImagesBy('custom', 'ASC');
 
-		return $objImageSorter->getImageUuids();
-	}
+        return $objImageSorter->getImageUuids();
+    }
 }
-
-?>
